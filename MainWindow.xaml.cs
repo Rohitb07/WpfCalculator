95b7111686f8a2eab07e4aa6cc801dbd0239c3d7
ROHIT BADGUJAR <badgujarrohit7@gmail.com>
2024-11-18 09:45:19 -0500

    wpf calculator

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,74 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfCalculator
+namespace CalculatorApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private string currentInput = "";
+        private string selectedOperator = "";
+        private double firstNumber = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string buttonValue = (sender as System.Windows.Controls.Button)?.Content.ToString();
+
+            if ("0123456789".Contains(buttonValue))
+            {
+                currentInput += buttonValue;
+                Display.Text = currentInput;
+            }
+            else if ("+-*/".Contains(buttonValue))
+            {
+                if (!string.IsNullOrEmpty(currentInput))
+                {
+                    firstNumber = double.Parse(currentInput);
+                    selectedOperator = buttonValue;
+                    currentInput = "";
+                }
+            }
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(currentInput) && !string.IsNullOrEmpty(selectedOperator))
+            {
+                double secondNumber = double.Parse(currentInput);
+                double calculationResult = 0;
+
+                switch (selectedOperator)
+                {
+                    case "+":
+                        calculationResult = firstNumber + secondNumber;
+                        break;
+                    case "-":
+                        calculationResult = firstNumber - secondNumber;
+                        break;
+                    case "*":
+                        calculationResult = firstNumber * secondNumber;
+                        break;
+                    case "/":
+                        calculationResult = secondNumber != 0 ? firstNumber / secondNumber : 0;
+                        break;
+                }
+
+                Display.Text = calculationResult.ToString();
+                currentInput = calculationResult.ToString();
+                selectedOperator = "";
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            currentInput = "";
+            selectedOperator = "";
+            firstNumber = 0;
+            Display.Text = "";
         }
     }
 }
